0b0e8bf18c3122d047945a22da5448720777b96c
julianduarteca <juaduarteca@gmail.com>
2022-10-11 22:41:55 -0500

    Initial config Spring Initializr

---
+++ DaviviendaCompras/src/main/java/Davivienda/DaviviendaCompras/DaviviendaComprasV1Application.java
@@ -0,0 +1,13 @@
+package Davivienda.DaviviendaCompras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaviviendaComprasV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaviviendaComprasV1Application.class, args);
+	}
+
+}
---
+++ DaviviendaCompras/src/test/java/Davivienda/DaviviendaCompras/DaviviendaComprasV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package Davivienda.DaviviendaCompras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaviviendaComprasV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
